7677cca39280572671e722c6f3fadacecdc520e7
milana-cat <103025089+milana-cat@users.noreply.github.com>
2023-09-25 19:58:21 +0300

    Добавлен класс таймера и возможность выбора файла с массивом

--- PythonSort.py
+++ PythonSort.py
@@ -1,8 +1,36 @@
 #!/usr/bin/env python
 #coding: utf-8 
+import time
 import numpy as np
 import os
-#import sys
+    
+class TimerError(Exception): 
+    """Пользовательское исключение, используемое для сообщения об ошибках при использовании класса Timer"""
+ 
+class Timer: 
+ 
+    def __init__(self):
+        self._start_time = None
+ 
+ 
+    def start(self):
+        """Запуск нового таймера"""
+ 
+        if self._start_time is not None:
+            raise TimerError(f"Таймер уже работает. Используйте .stop() чтобы его остановить")
+ 
+        self._start_time = time.perf_counter()
+ 
+ 
+    def stop(self):
+        """Отстановить таймер и сообщить о времени вычисления"""
+ 
+        if self._start_time is None:
+            raise TimerError(f"Таймер не работает. Используйте .start() для его запуска")
+ 
+        elapsed_time = time.perf_counter() - self._start_time
+        self._start_time = None
+        print(f"Вычисление заняло {elapsed_time:0.4f} секунд")
 
 def insertion_sort(nums):  
     # Сортировку начинаем со второго элемента, т.к. считается, что первый элемент уже отсортирован
@@ -35,13 +63,14 @@ def OpenFile(file_):
 def readArrFile(filename, array):
    f=open(filename,encoding='utf-8')
    for line in f:
+    print(int(x) for x in line.split())
     array.append([int(x) for x in line.split()])
     #array.append(int(f.readline()))
 
 def writeFile(fileName, array):
     resultFile=open(fileName,'w+',encoding = 'utf-8')
     for i in array:
-                #print(str(i))
+                print(str(i))
                 resultFile.write(str(i)+"\n")
                 #resultFile.write("\n")
     resultFile.close()
@@ -51,69 +80,76 @@ while(i!=2):
     i=int(input())
     if i==2:
         exit()
-    #Проверка введённых длины, максимального и минимального значений массива
-    print("\n Array length:")
-    try:
-        length=int(input())
-    except ValueError as err:
-        print("Could not convert data to an integer.")
+    print("Create a new file with an array? 1. Yes 2. No")
+    j=int(input())
+    if(j==1):
+            #Проверка введённых длины, максимального и минимального значений массива
+        print("\n Array length:")
+        try:
+            length=int(input())
+        except ValueError as err:
+            print("Could not convert data to an integer.")
+            continue
+        except Exception as err:
+            print(err)
+            continue
+        print("\n Minimum array element:")
+        try:
+            min_num=int(input())
+        except ValueError as err:
+            print("Could not convert data to an integer.")
+            continue
+        except Exception as err:
+            print(err)
+            continue
+        print("\n Maximum array element:")
+        try:
+            max_num=int(input())
+        except ValueError as err:
+            print("Could not convert data to an integer.")
+            continue
+        except Exception as err:
+            print(err)
+        if min_num>max_num:
+            print("The minimum value cannot be greater than the maximum.")
+            continue
+        #заполнение массива
+        SortArr=[]
+        SortArr=np.random.randint(min_num,max_num,length)
+        #Запись массива в файл
+        print("\nThe name of the file with the unsorted array (Example: filename ):")
+        name=str(input()+".txt")
+        try:
+            #FileName=OpenFile(name)
+            writeFile(name,SortArr)#FileName,SortArr)
+        except Exception as err:
+            print("Failed to create and fill in the file. Error:"+ err)
+            continue
+    elif(j==2):
+        print("\nThe name of the file with the unsorted array (Example: filename ):")
+        name=str(input()+".txt")
+        SortArr=[]
+        #читаем рандомный массив из файла, в который его записали
+        try:
+            readArrFile(name,SortArr)
+        except Exception as err:
+            print("Could not read the file. Error:"+ err)
+            continue
+    else:
+        print("Input error")
         continue
-    except Exception as err:
-        print(err)
-        continue
-    print("\n Minimum array element:")
-    try:
-        min_num=int(input())
-    except ValueError as err:
-        print("Could not convert data to an integer.")
-        continue
-    except Exception as err:
-        print(err)
-        continue
-    print("\n Maximum array element:")
-    try:
-        max_num=int(input())
-    except ValueError as err:
-        print("Could not convert data to an integer.")
-        continue
-    except Exception as err:
-        print(err)
-    if min_num>max_num:
-        print("The minimum value cannot be greater than the maximum.")
-        continue
-    #заполнение массива
-    SortArr=[]
-    SortArr=np.random.randint(min_num,max_num,length)
-    #Запись массива в файл
-    print("\nThe name of the file with the unsorted array (Example: filename ):")
-    name=str(input()+".txt")
-    try:
-        #FileName=OpenFile(name)
-        writeFile(name,SortArr)#FileName,SortArr)
-    except Exception as err:
-        print("Failed to create and fill in the file. Error:"+ err)
-        continue
-    
-    SortArr=[]
-    #читаем рандомный массив из файла, в который его записали
-    try:
-        #FileName=OpenFile(name)
-        #FileName=OpenFile("test.txt")
-        readArrFile(name,SortArr)
-    except Exception as err:
-        print("Could not read the file. Error:"+ err)
-        continue
+    #Замерка времени сортировки
+    t=Timer()
+    t.start()
     #Сортировка массива
-    insertion_sort(SortArr)  
+    insertion_sort(SortArr) 
+    t.stop()
     #Запись отсортированного массива в файл
     print("\nThe name of the file with the sorted array (Example: filename ):")
     name=str(input()+".txt")
     try:
-        #FileName=OpenFile(name)
-        
         writeFile(name,SortArr)
     except Exception as err:
         print("Failed to create and fill in the file. Error:"+ err)
         continue
-    
-    #print(SortArr)
+
